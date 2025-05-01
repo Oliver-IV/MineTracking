fbad74fa8a86f76452b817087e3b9ad9e73ef099
luisa <moraLF20@outlook.com>
2025-05-01 00:58:22 -0700

    finishing connection config with api gateway

--- mvts-api-gateway/src/cars/cars.module.ts
+++ mvts-api-gateway/src/cars/cars.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CarsController } from './cars.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { CARS_SERVICE } from './constants';
+import { join } from 'path';
+import { protobufPackage } from './proto/cars';
 
 @Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: CARS_SERVICE,
+        transport: Transport.GRPC,
+        options: {
+          package: protobufPackage,
+          protoPath: join(__dirname, '../cars/proto/cars.proto'),
+        },
+      },
+    ]),
+  ],
   controllers: [CarsController],
   providers: [CarsService],
 })
