d0a4627a929ba627898c82219c1545aa0d1f30a7
luisa <moraLF20@outlook.com>
2025-05-03 22:19:18 -0700

    deleting old project

--- mvts-assets-manager/src/traffic-lights/traffic-lights.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TrafficLightsService } from './traffic-lights.service';
-import { TrafficLightsController } from './traffic-lights.controller';
-
-@Module({
-  controllers: [TrafficLightsController],
-  providers: [TrafficLightsService],
-})
-export class TrafficLightsModule {}
