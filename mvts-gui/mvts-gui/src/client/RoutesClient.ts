a589a4ea201f954c023664e18159530640f3b2ff
Oliver-IV <145882378+Oliver-IV@users.noreply.github.com>
2025-05-19 16:47:28 -0700

    RoutesClient done (Not tested yet)

--- mvts-gui/mvts-gui/src/client/RoutesClient.ts
+++ mvts-gui/mvts-gui/src/client/RoutesClient.ts
@@ -1,26 +1,96 @@
-import { Route } from "../types/back/routeDto/get-route.dto"
+import { HOST_NAME } from "@/configs/configs";
+import { CreateRouteDto } from "@/types/back/routeDto/create-route.dto";
+import { LocationDTO } from "@/types/back/routeDto/location.dto";
+import { Route } from "@/types/front/Route";
+import { Update } from "vite/types/hmrPayload";
 
-async function GETfindAllRoutes() : Route[]{
-    const response = await fetch(
-        `${HOST_NAME}/routes`,
-        {
-            method: "GET"
+async function GETfindAllRoutes(): Promise<Route[]> {
+    try {
+        const response = await fetch(`${HOST_NAME}/routes`);
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
         }
-    ) ;
+        const routes: Route[] = await response.json();
+        return routes;
+    } catch (error) {
+        console.error("Failed to get routes:", error);
+    }
 }
 
-async function GETfindAllLocations() {
 
+async function GETfindAllLocations(): Promise<LocationDTO[]> {
+    try {
+        const response = await fetch(`${HOST_NAME}/routes/locations`);
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        const locations: LocationDTO[] = await response.json();
+        return locations;
+    } catch (error) {
+        console.error("Failed to get routes:", error);
+    }
 }
 
-async function POSTcreateRoute() {
-
+async function POSTcreateRoute(route: CreateRouteDto): Promise<Route> {
+    try {
+        const response = await fetch(`${HOST_NAME}/routes`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(route)
+        });
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        return response.json();
+    } catch (error) {
+        console.error("Failed to get routes:", error);
+    }
 }
 
-async function PATCHupdateRoute() {
-
+async function PATCHupdateRoute(routeId: string,locationIds: { startId: string, endId: string }) {
+    try {
+        const route = {};
+        if (!locationIds) {
+            throw new Error("There are no locations to update");
+        }
+        if (locationIds.startId) {
+            route["startId"] = locationIds.startId;
+        }
+        if (locationIds.endId) {
+            route["endId"] = locationIds.endId;
+        }
+        const response = await fetch(`${HOST_NAME}/routes/${routeId}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(route)
+        });
+        if (!response.ok) {
+            throw new Error(`Error ${response.status}: ${response.statusText}`);
+        }
+        return response.json();
+    } catch (error) {
+        console.error("Failed to get routes:", error);
+    }
 }
 
-async function DELETEdeleteRoute() {
-
+async function DELETEdeleteRoute(routeId: string) {
+    if (!routeId) {
+        throw new Error("There is no location to delete");
+    }
+    const response = await fetch(`${HOST_NAME}/routes/${routeId}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    });
+    if (!response.ok) {
+        throw new Error(`Error ${response.status}: ${response.statusText}`);
+    }
+    return response.json();
 }
+
+export { GETfindAllRoutes, GETfindAllLocations, POSTcreateRoute, PATCHupdateRoute, DELETEdeleteRoute }
