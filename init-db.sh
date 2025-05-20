8a2b22daefe5391447523f68508ee77ef50c37a8
José Karim Franco Valencia <karimfv04@gmail.com>
2025-05-20 08:49:43 -0700

    Shipment service dockerize

--- init-db.sh
+++ init-db.sh
@@ -21,14 +21,3 @@ if [  -n "${POSTGRES_MULTIPLE_DATABASES:-}" ]; then
     done
     echo "Multiple databases created"
 fi
-
-#########################version 1############################
-# set -e
-
-# psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-#   CREATE DATABASE mvts_car_db;
-#   CREATE DATABASE mvts_traffic_lights_db;
-  
-#   GRANT ALL PRIVILEGES ON DATABASE mvts_car_db TO $POSTGRES_USER;
-#   GRANT ALL PRIVILEGES ON DATABASE mvts_traffic_lights_db TO $POSTGRES_USER;
-# EOSQL
--- mtvs_shipment_service/Program.cs
+++ mtvs_shipment_service/Program.cs
@@ -16,6 +16,48 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgreSQLConnection")));
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var context = services.GetRequiredService<AppDbContext>();
+
+        // Intentar varias veces por si la base de datos a�n no est� lista
+        var retry = 0;
+        var maxRetries = 10;
+        Console.WriteLine("Intentando aplicar migraciones a la base de datos...");
+
+        while (retry < maxRetries)
+        {
+            try
+            {
+                context.Database.Migrate();
+                Console.WriteLine("Migraci�n de base de datos aplicada con �xito");
+                break; // Si tiene �xito, sal del bucle
+            }
+            catch (Exception ex)
+            {
+                retry++;
+                if (retry >= maxRetries)
+                {
+                    Console.WriteLine($"No se pudo migrar la base de datos despu�s de {maxRetries} intentos. �ltimo error: {ex.Message}");
+                    throw;
+                }
+
+                Console.WriteLine($"Error al migrar la base de datos (intento {retry}/{maxRetries}): {ex.Message}");
+                // Espera antes de reintentar
+                Thread.Sleep(5000); // 5 segundos
+            }
+        }
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "Error al aplicar las migraciones");
+        throw;
+    }
+}
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<ShipmentGrpcService>();
