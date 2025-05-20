245f3150c0e4c616c6a9e7c3c9cb1d5c07a330c7
Oliver-IV <145882378+Oliver-IV@users.noreply.github.com>
2025-05-19 22:00:34 -0700

    Reports Client and Auth Client done

--- mvts-gui/mvts-gui/src/client/AuthClient.ts
+++ mvts-gui/mvts-gui/src/client/AuthClient.ts
@@ -1,13 +1,61 @@
-async function POTSlogin() {
+import { HOST_NAME } from "@/configs/configs";
+import type { LoginDto } from "@/types/back/authDto/login.dto";
+import type { RegisterUserDto } from "@/types/back/authDto/register-user.dto";
 
+async function POTSlogin(loginDto: LoginDto) {
+    try {
+        const response = await fetch(`${HOST_NAME}/auth/login`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(loginDto),
+        });
+        if (response.status === 401) {
+            throw new Error("Wrong Credentials");
+        }
+        if (!response.ok) {
+            throw new Error("Error logging in");
+        }
+        return response.json();
+    } catch (error) {
+        console.error("Er:", error);
+        throw error;
+    }
 }
 
-async function POSTregister() {
-
+async function POSTregister(registerUserDto: RegisterUserDto): Promise<any> {
+    try {
+        const response = await fetch(`${HOST_NAME}/auth/register`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(registerUserDto),
+        });
+        if (!response.ok) {
+            throw new Error("Error logging in");
+        }
+        return response.json();
+    } catch (error) {
+        console.error("Error comunicating with the server:", error);
+        throw error;
+    }
 }
 
 async function POSTlogout() {
-
+    try {
+        const response = await fetch(`${HOST_NAME}/auth/logout`, {
+            method: "POST"
+        });
+        if (!response.ok) {
+            throw new Error("Error logging out");
+        }
+        return response.json();
+    } catch (error) {
+        console.error("Error comunicating with the server:", error);
+        throw error;
+    }
 }
 
-export { POTSlogin, POSTregister, POSTlogout } ;
+export { POTSlogin, POSTregister, POSTlogout };
--- mvts-gui/mvts-gui/src/client/ReportsClient.ts
+++ mvts-gui/mvts-gui/src/client/ReportsClient.ts
@@ -1,5 +1,24 @@
-async function POSTgeneratePDF() {
-
+import { HOST_NAME } from "@/configs/configs";
+import type { ReportDetailsDTO } from "../types/back/reportsDto/report.detailts.dto";
+async function POSTgeneratePDF(reportsDetails: ReportDetailsDTO): Promise<void> {
+    try {
+        const response = await fetch(`${HOST_NAME}/api/reports/generatePDF`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(reportsDetails),
+        });
+        if (!response.ok) {
+            throw new Error(`HTTP error ${response.status}: ${response.statusText}`);
+        }
+        const blob = await response.blob();
+        const pdfUrl = window.URL.createObjectURL(blob);
+        window.open(pdfUrl, '_blank');
+    } catch (error) {
+        console.error("Error comunicating with the server:", error);
+        throw error;
+    }
 }
 
-export { POSTgeneratePDF } ;
+export { POSTgeneratePDF };
